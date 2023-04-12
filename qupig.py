d2fca41f232e685354fe3806018fc6177f69879e
jpmarceaux <johnpaulmarceaux@gmail.com>
2023-04-12 11:41:30 -0700

    initial upload

---
+++ qupig.py
@@ -0,0 +1,7 @@
+pygsti_to_qubic = {
+        'Gxpi2': 'X90',
+        'Gcr': 'CR',
+        'Gzpi2': 'Z90',
+        'Gxx': ['X90', 'X90'],
+        'Gxy': ['X90', 'Y90'],
+        'Gyx': ['Y90', 'X90']}
